1113a3d50ebf11dba9213fdcbadb96aa3021abba
ranjith <ranjithkumarcena@gmail.com>
2023-06-22 13:52:17 -0400

    cart page done

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -17,6 +17,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100, null=True )
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=False)
+    #this requires the pillow lib
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
@@ -42,12 +43,28 @@ class Order(models.Model):
     def __str__(self):
         return str(self.id)
     
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+    
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+    
 class OrderItem(models.Model):
 
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     data_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+
+        total = self.product.price * self.quantity
+        return total
 
 
 class ShippingAddress(models.Model):
